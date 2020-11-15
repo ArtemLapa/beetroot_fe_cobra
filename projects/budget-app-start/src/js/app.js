626317f9bd56559a250b345379cb91e22375c9b8
ArtemLapa <artyom.lapa@gmail.com>
2020-11-15 12:40:19 +0200

    :tada: budget app

---
+++ projects/budget-app-start/src/js/app.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const balance = document.getElementById('balance');
+  const incomeMoney = document.getElementById('money-plus');
+  const expenseMoney = document.getElementById('money-minus');
+  const historyList = document.getElementById('list');
+  const textInput = document.getElementById('text');
+  const amountInput = document.getElementById('amount');
+  const transactionBtn = document.getElementById('add-btn');
+});
